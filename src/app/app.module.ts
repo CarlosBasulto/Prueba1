48d088b7284e352e59ad12330918cf1ecc818174
Carlos Basulto Pardo <136237882+CarlosBasulto@users.noreply.github.com>
2024-12-03 11:43:22 +0100

    NUevo componenete y servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductoComponent } from './producto/producto.component';
+import { Producto2Component } from './producto2/producto2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductoComponent
+    ProductoComponent,
+    Producto2Component
   ],
   imports: [
     BrowserModule,
